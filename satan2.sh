6e0dfe1347e580cb42828d98986f530d57358c34
Franck FERMAN <73023545+franckferman@users.noreply.github.com>
2022-09-11 13:20:41 +0200

    Add files via upload

--- satan2.sh
+++ satan2.sh
@@ -6,6 +6,7 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 error=77
+success=0
 
 Check_AdminRights()
 {
@@ -15,7 +16,7 @@ uid_root=0
         then
         echo "${red}Error, please run the script with sudo.${reset}"
         exit $error
-    
+        
     elif [ "$UID" -eq "$uid_root" ]
         then
         main
@@ -32,7 +33,7 @@ Ask_Yes_Or_No()
 {
 read -p "Are you sure you want to continue (nN/yY): " answer
 case $answer in
-[Yy]* ) continue;;
+[Yy]* ) ;;
 [Nn]* ) echo;echo "Good luck to you.";exit$success;;
 * ) echo "${red}A choice is required.${reset}";echo;Ask_Yes_Or_No
 esac
@@ -50,7 +51,6 @@ Ask_Yes_Or_No
 
 Check_dependencies()
 {
-success=0
 satan2dir=/mnt/satan2
 
 clear
@@ -62,6 +62,15 @@ else
     echo "${red}zuluCrypt-cli not found.${reset}"
     exit $error
 fi
+
+if command -v cryptsetup>/dev/null 2>&1;then
+    echo "${green}cryptsetup found.${reset}"
+else
+    echo "${red}Error, missing dependencies.${reset}"
+    echo "${red}cryptsetup not found.${reset}"
+    exit $error
+fi
+echo;echo
 }
 
 Get_Banner()
@@ -103,6 +112,8 @@ echo "List of disks: ";echo"";lsblk -d|tail -n+2|awk '{print $1" "$4}'|nl -v 0 -
 disks=( $(lsblk -d|tail -n+2|awk '{print $1}') )
 echo "${green}${disks[userchoice]} ${reset}selected."
 
+echo;Ask_Yes_Or_No
+
 echo;echo "Disk unmounting."
 sudo umount /dev/${disks[userchoice]}
 
@@ -119,7 +130,7 @@ else
 fi
 
 echo;echo "Disk mounting."
-sudo mount $disk /dev/${disks[userchoice]} $satan2dir
+sudo mount /dev/${disks[userchoice]} $satan2dir
 
 echo;echo "Overwriting of data."
 dd if=/dev/urandom of=/dev/${disks[userchoice]} bs=4096 status=progress
@@ -156,7 +167,7 @@ else
 fi
 
 echo;echo "Disk mounting."
-sudo mount $disk /dev/${disks[userchoice]} $satan2dir
+sudo mount /dev/${disks[userchoice]} $satan2dir
 
 echo;echo "Overwriting of data."
 dd if=/dev/urandom of=/dev/${disks[userchoice]} bs=4096 status=progress
@@ -198,11 +209,10 @@ else
 fi
 
 echo;echo "Disk mounting."
-sudo mount $disk /dev/${disks[userchoice]} $satan2dir
+sudo mount /dev/${disks[userchoice]} $satan2dir
 
 echo;echo "Overwriting of data."
 dd if=/dev/urandom of=/dev/${disks[userchoice]} bs=4096 status=progress
-
 shred -n 2 -z -v /dev/${disks[userchoice]}
 
 echo;echo "Disk unmounting."
@@ -232,6 +242,68 @@ sudo zuluCrypt-cli -c -d ./satan2.key -z ext4 -t luks -p $passwd -f $OUTFILE -k
 
 echo;echo "Overwrite the used keyfile."
 shred -n 8 -z -v ./satan2.key
+
+echo;echo "Disk unmounting."
+sudo umount /dev/${disks[userchoice]}
+
+echo;echo "Disk formating."
+sudo mkfs -t ext4 /dev/${disks[userchoice]} <<<y
+
+if [[ ! -e $satan2dir ]]; then
+    sudo mkdir -p $satan2dir
+else 
+    sudo rm -Rf $satan2dir
+    sudo mkdir -p $satan2dir
+fi
+
+echo;echo "Disk mounting."
+sudo mount /dev/${disks[userchoice]} $satan2dir
+
+echo;echo "Overwriting of data."
+dd if=/dev/urandom of=/dev/${disks[userchoice]} bs=4096 status=progress
+shred -n 2 -z -v /dev/${disks[userchoice]}
+
+passwd="$(openssl rand -base64 42 | tr -dc 'a-zA-Z0-9!@#$%^&*()_+-=')"
+passwd="$(echo $passwd|rev)"
+rand=$[($RANDOM%10)+1]
+passwd="$(echo $passwd|cut -c ${rand}-)"
+
+passwd2="$(echo $passwd|base64)"
+passwd2="$(echo $passwd2|rev)"
+rand=$[($RANDOM%32)+1]
+passwd2="$(echo $passwd2|cut -c ${rand}-)"
+passwd=$passwd2$passwd
+passwd="$(echo $passwd|rev)"
+rand=$[($RANDOM%10)+1]
+passwd="$(echo $passwd|cut -c ${rand}-)"
+
+echo;echo "Disk unmounting."
+sudo umount $disk /dev/${disks[userchoice]}
+
+echo;echo "Encryption of the hard disk using cryptsetup."
+echo $passwd|sudo cryptsetup -y -v luksFormat /dev/${disks[userchoice]}
+passwd="$(echo $passwd|rev)"
+echo $passwd|sudo cryptsetup -y -v luksFormat /dev/${disks[userchoice]}
+
+echo;echo "Disk formating."
+sudo mkfs -t ext4 /dev/${disks[userchoice]} <<<y
+
+if [[ ! -e $satan2dir ]]; then
+    sudo mkdir -p $satan2dir
+else
+    sudo rm -Rf $satan2dir
+    sudo mkdir -p $satan2dir
+fi
+
+echo;echo "Disk mounting."
+sudo mount /dev/${disks[userchoice]} $satan2dir
+
+echo;echo "Overwriting of data."
+dd if=/dev/urandom of=/dev/${disks[userchoice]} bs=4096 status=progress
+shred -n 2 -z -v /dev/${disks[userchoice]}
+
+echo;echo "Disk unmounting."
+sudo umount /dev/${disks[userchoice]}
 
 exit $success
 }
